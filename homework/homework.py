b5542cc66dabd2592d12d850f89ec32d7df7e2f5
Luisa Rendon <lurendong@unal.edu.co>
2026-07-12 22:37:17 -0500

    intento 1

--- homework/homework.py
+++ homework/homework.py
@@ -95,3 +95,164 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+import gzip
+import json
+import os
+import pickle
+
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    balanced_accuracy_score,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+)
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+
+def load_data():
+    train = pd.read_csv("files/input/train_data.csv.zip")
+    test = pd.read_csv("files/input/test_data.csv.zip")
+
+    for df in (train, test):
+        df.rename(
+            columns={"default payment next month": "default"},
+            inplace=True,
+        )
+        df.drop(columns=["ID"], inplace=True)
+
+    train = train[(train["EDUCATION"] != 0) & (train["MARRIAGE"] != 0)].copy()
+    test = test[(test["EDUCATION"] != 0) & (test["MARRIAGE"] != 0)].copy()
+
+    train["EDUCATION"] = train["EDUCATION"].apply(lambda x: 4 if x > 4 else x)
+    test["EDUCATION"] = test["EDUCATION"].apply(lambda x: 4 if x > 4 else x)
+
+    return train, test
+
+
+def create_pipeline():
+    categorical_features = ["SEX", "EDUCATION", "MARRIAGE"]
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "cat",
+                OneHotEncoder(handle_unknown="ignore"),
+                categorical_features,
+            ),
+        ],
+        remainder=MinMaxScaler(),
+    )
+
+    pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("selectkbest", SelectKBest(score_func=f_classif)),
+            (
+                "logreg",
+                LogisticRegression(
+                    max_iter=1000,
+                    random_state=12345,
+                ),
+            ),
+        ]
+    )
+
+    param_grid = {
+        "selectkbest__k": [10, 12, 14, 16, 18, 20],
+        "logreg__C": [0.01, 0.1, 1.0, 10.0],
+        "logreg__solver": ["lbfgs"],
+    }
+
+    model = GridSearchCV(
+        pipeline,
+        param_grid=param_grid,
+        cv=10,
+        scoring="balanced_accuracy",
+        n_jobs=-1,
+    )
+
+    return model
+
+
+def save_model(model):
+    os.makedirs("files/models", exist_ok=True)
+
+    with gzip.open("files/models/model.pkl.gz", "wb") as file:
+        pickle.dump(model, file)
+
+
+def metrics_record(dataset, y_true, y_pred):
+    return {
+        "type": "metrics",
+        "dataset": dataset,
+        "precision": round(precision_score(y_true, y_pred), 3),
+        "balanced_accuracy": round(
+            balanced_accuracy_score(y_true, y_pred),
+            3,
+        ),
+        "recall": round(recall_score(y_true, y_pred), 3),
+        "f1_score": round(f1_score(y_true, y_pred), 3),
+    }
+
+
+def cm_record(dataset, y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+
+    return {
+        "type": "cm_matrix",
+        "dataset": dataset,
+        "true_0": {
+            "predicted_0": int(cm[0, 0]),
+            "predicted_1": int(cm[0, 1]),
+        },
+        "true_1": {
+            "predicted_0": int(cm[1, 0]),
+            "predicted_1": int(cm[1, 1]),
+        },
+    }
+
+
+def save_metrics(metrics):
+    os.makedirs("files/output", exist_ok=True)
+
+    with open("files/output/metrics.json", "w", encoding="utf-8") as file:
+        for metric in metrics:
+            file.write(json.dumps(metric) + "\n")
+
+
+def main():
+    train, test = load_data()
+
+    x_train = train.drop(columns=["default"])
+    y_train = train["default"]
+
+    x_test = test.drop(columns=["default"])
+    y_test = test["default"]
+
+    model = create_pipeline()
+    model.fit(x_train, y_train)
+
+    save_model(model)
+
+    y_train_pred = model.predict(x_train)
+    y_test_pred = model.predict(x_test)
+
+    metrics = [
+        metrics_record("train", y_train, y_train_pred),
+        metrics_record("test", y_test, y_test_pred),
+        cm_record("train", y_train, y_train_pred),
+        cm_record("test", y_test, y_test_pred),
+    ]
+
+    save_metrics(metrics)
+
+
+if __name__ == "__main__":
+    main()
